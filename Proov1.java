1b0174a3580c8408bc7f1d32ca81e60bba925db4
ophyneus <ophyneus@tlu.ee>
2015-03-13 11:09:19 +0200

    Takisti klass koos prooviga

---
+++ Proov1.java
@@ -0,0 +1,7 @@
+public class Proov1{
+	public static void main(String[] arg){
+		TakistusKomponent t=new Takisti220oomi();
+		System.out.println(t.kysiTakistus());
+		System.out.println("vool 9 voldi korral: "+t.vool(9));
+	}
+}
