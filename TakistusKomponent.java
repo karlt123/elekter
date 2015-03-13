782d7253b33ec5defc946a5dbebc8af7b0ffd60d
ophyneus <ophyneus@tlu.ee>
2015-03-13 10:58:22 +0200

    Parameetri komentaar

--- TakistusKomponent.java
+++ TakistusKomponent.java
@@ -1,5 +1,9 @@
 abstract class TakistusKomponent{
 	abstract double kysiTakistus();
+	/**
+	*	Leitakse etteantud pingele vastav vool
+	*	@param pinge - rakendatav pinge voltides
+	*/
 	public double vool(double pinge){
 		return pinge/kysiTakistus();
 	}
